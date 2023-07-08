f6dbe374d71109cf49d112b77dd1a8bce8b4e25b
Aditya Singh <126137304+ADITYA2004S@users.noreply.github.com>
2023-07-08 19:21:42 +0530

    Full Product
    Use this application to encode you important message and decode them

---
+++ Caesar Cipher.py
@@ -0,0 +1,47 @@
+
+logo = """
+ _____                              _____ _       _
+  / ____|                            / ____(_)     | |
+ | |     __ _  ___  ___  __ _ _ __  | |     _ _ __ | |__   ___ _ __
+ | |    / _` |/ _ \/ __|/ _` | '__| | |    | | '_ \| '_ \ / _ \ '__|
+ | |___| (_| |  __/\__ \ (_| | |    | |____| | |_) | | | |  __/ |
+  \_____\__,_|\___||___/\__,_|_|     \_____|_| .__/|_| |_|\___|_|
+                                             | |
+                                             |_|
+"""
+print(logo)
+
+
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+text = input("Type your message:\n").lower()
+shift = int(input("Type the shift number:\n"))
+
+
+#ENCRYPTION CODE
+def encrypt(Plain_text, Shift_amount):
+    Cipher_text = ""
+    for letter in Plain_text:
+        Position = alphabet.index(letter)
+        New_Position = Position + Shift_amount
+        New_Letter = alphabet[New_Position]
+        Cipher_text+=New_Letter
+    print(f"The encoded text is {Cipher_text}")
+
+
+#DECRYPTION CODE
+def decrypt(Cipher_text, Shift_amount):
+    Plain_text = ""
+    for letter in Cipher_text:
+        Position = alphabet.index(letter)
+        New_positin = Position - Shift_amount
+        Plain_text+=alphabet[New_positin]
+    print(f"The decoded text is {Plain_text}")
+
+if direction == "encode":
+    encrypt(Plain_text=text, Shift_amount=shift)
+elif direction == "decode":
+    decrypt(Cipher_text=text, Shift_amount=shift)
+else:
+    print("Acha lauda apne man se dalega !!! wahhh")
